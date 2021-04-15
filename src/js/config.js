03f6cc4e740e351d5a73ec64c3821e6ef6fdccea
Sir-Unkie <unknown920@gmail.com>
2021-04-15 18:02:11 +0300

    qqqq

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,7 @@
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime'
-export const API_URL = 'https:forkify-api.herokuapp.com/api/v2/recipes';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -14,7 +14,7 @@ const timeout = function (s) {
 export const getJSON = async function (URL) {
     try {
         console.log('URL2', URL);
-        const res = await Promise.race([fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/?search=pizza&key=3372ab01-dff1-4333-8bdf-020c866cf664`), timeout(TIMEOUT_SEC)]);
+        const res = await Promise.race([fetch(URL), timeout(TIMEOUT_SEC)]);
         console.log('res', res);
         const data = await res.json();
         console.log('data: ', data);
