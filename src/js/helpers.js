d6020a3365d00f1979cacff33bc9f4b42fd3dd0f
Sir-Unkie <unknown920@gmail.com>
2021-04-15 16:16:41 +0300

    asdasdasd

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -17,6 +17,7 @@ export const getJSON = async function (URL) {
         const res = await Promise.race([fetch(URL), timeout(TIMEOUT_SEC)]);
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message}, status: ${res.status}`);
+        console.log('data: ', data);
         return data;
     } catch (err) {
         throw err
